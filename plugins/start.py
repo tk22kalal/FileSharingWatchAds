360d0ea3d5d44de0119ce2bd88042371df844904
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-08 16:03:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,9 +55,14 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("Please wait Baby...")
         try:
             messages = await get_messages(client, ids)
+            await message.reply_text(
+            f"Files will be deleted in 2 minutes to avoid copyright issues.",
+            disable_web_page_preview=True,
+            quote=True
+        )
         except:
             await message.reply_text("Something went wrong..!")
             return
