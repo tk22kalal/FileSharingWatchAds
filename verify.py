909e7fc81ee76f41fde5f5fe092a1d4542ae3ba9
zenox5317 <146443137+zenox5317@users.noreply.github.com>
2023-10-11 16:15:46 +0530

    Create verify.py

---
+++ verify.py
@@ -0,0 +1,67 @@
+import os
+import string
+import random
+import pytz
+from datetime import date
+import requests as re
+
+SHORTNER = os.environ.get("SHORTENER_SITE")
+API = os.environ.get("SHORTENER_API")
+
+async def get_shortlink(link, message):
+    res = re.get(f'https://{SHORTNER}/api?api={API}&url={link}')
+    res.raise_for_status()
+    data = res.json()
+    return data.get('shortenedUrl')
+
+async def generate_random_string(num: int):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choice(characters) for _ in range(num))
+    return random_string
+
+TOKENS = {}
+VERIFIED = {}
+
+async def check_token(bot, userid, token):
+    user = await bot.get_users(userid)
+    if user.id in TOKENS.keys():
+        TKN = TOKENS[user.id]
+        if token in TKN.keys():
+            is_used = TKN[token]
+            if is_used == True:
+                return False
+            else:
+                return True
+    else:
+        return False
+
+
+async def get_token(bot, userid, link):
+    user = await bot.get_users(userid)
+    token = await generate_random_string(7)
+    TOKENS[user.id] = {token: False}
+    link = f"{link}verify-{user.id}-{token}"
+    shortened_verify_url = await get_shortlink(link)
+    return str(shortened_verify_url)
+
+async def verify_user(bot, userid, token):
+    user = await bot.get_users(userid)
+    TOKENS[user.id] = {token: True}
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    VERIFIED[user.id] = str(today)
+
+async def check_verification(bot, userid):
+    user = await bot.get_users(userid)
+    tz = pytz.timezone('Asia/Kolkata')
+    today = date.today()
+    if user.id in VERIFIED.keys():
+        EXP = VERIFIED[user.id]
+        years, month, day = EXP.split('-')
+        comp = date(int(years), int(month), int(day))
+        if comp < today:
+            return False
+        else:
+            return True
+    else:
+        return False
