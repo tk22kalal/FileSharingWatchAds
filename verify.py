2f4cd783ed92682b70353e9bf368f43c692a2faf
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-29 03:07:36 +0530

    Update verify.py

--- verify.py
+++ verify.py
@@ -14,13 +14,17 @@ async def get_shortlink(link):
     data = res.json()
     return data.get('shortenedUrl')
 
-async def generate_random_string(num: int):
-    characters = string.ascii_letters + string.digits
-    random_string = ''.join(random.choice(characters) for _ in range(num))
-    return random_string
-
 TOKENS = {}
 VERIFIED = {}
+
+
+async def generate_random_string(num: int):
+    if num == 28:
+        characters = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(characters) for _ in range(num))
+        return random_string
+    else:
+        raise ValueError("The 'num' parameter must be 28 for generating a 28-character string.")
 
 async def check_token(bot, userid, token):
     user = await bot.get_users(userid)
@@ -38,9 +42,9 @@ async def check_token(bot, userid, token):
 
 async def get_token(bot, userid, link):
     user = await bot.get_users(userid)
-    token = await generate_random_string(8)
+    token = await generate_random_string(28)
     TOKENS[user.id] = {token: False}
-    link = f"{link}verify-{user.id}-{token}"
+    link = f"{link}{token}"
     shortened_verify_url = await get_shortlink(link)
     return str(shortened_verify_url)
 
