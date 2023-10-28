1af37b78369618049741ee7e9b439f9347e9006f
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-29 00:17:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,6 +21,7 @@ VERIFY = "True"
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
+    user_id = message.from_user.id
     id = message.from_user.id
     if not await present_user(id):
         try:
