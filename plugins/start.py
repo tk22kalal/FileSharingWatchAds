d843abc7cd8e22c51a4bc1618af9332aa8accc73
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-29 04:15:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -142,8 +142,8 @@ async def start_command(client: Client, message: Message):
         )
         await asyncio.sleep(25)
         await arg.delete()
-        return
-    else
+    return
+    
         reply_markup = InlineKeyboardMarkup(
             [
                 [
