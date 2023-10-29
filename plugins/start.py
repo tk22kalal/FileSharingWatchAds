294dcce8c7255c128ed2aee27173f84286c15f57
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-29 18:40:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -225,7 +225,7 @@ async def not_joined(client: Client, message: Message):
             [
                 InlineKeyboardButton(
                     text = 'Try Again',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                    url = f"https://t.me/{client.username}?start={message.command[2]}"
                 )
             ]
         )
